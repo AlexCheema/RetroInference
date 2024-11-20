1c28b814accca11ba085388365fbad9833bcaa71
Matt Beton <matthew.beton@gmail.com>
2024-11-20 15:07:45 +0700

    Successfully doing some generation.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <iostream>
 
+
 namespace fs = std::filesystem;
 
 std::string getRootDir(fs::path path)
@@ -17,7 +18,7 @@ std::string getRootDir(fs::path path)
 std::string getModelFile(fs::path path)
 {
     path = getRootDir(path);
-    path.append("models/gru_torch.json");
+    path.append("models/generative_gru.json");
 
     return path.string();
 }
@@ -25,45 +26,130 @@ std::string getModelFile(fs::path path)
 std::string getInputFile(fs::path path)
 {
     path = getRootDir(path);
-    path.append("test_data/gru_torch_x_python.csv");
+    path.append("test_data/generative_gru_x.csv");
     return path.string();
 }
 
 std::string getOutputFile(fs::path path)
 {
     path = getRootDir(path);
-    path.append("test_data/gru_torch_y_python.csv");
+    path.append("test_data/generative_gru_y.csv");
     return path.string();
 }
 
-constexpr int gru_input_size = 1;
-constexpr int gru_hidden_size = 8;
-constexpr int n_output_neurons = 1;
+constexpr int vocab_size = 27;
+constexpr int embed_size = 128;
+constexpr int hidden_size = 256;
 
-using ModelType = RTNeural::ModelT<float, gru_input_size, n_output_neurons,
-    RTNeural::GRULayerT<float, gru_input_size, gru_hidden_size>,
-    RTNeural::GRULayerT<float, gru_hidden_size, gru_hidden_size>,
-    RTNeural::DenseT<float, gru_hidden_size, n_output_neurons>>;
+const std::unordered_map<int, char> intToCharMap = {
+    {0, ' '}, {1, 'a'}, {2, 'b'}, {3, 'c'}, {4, 'd'}, {5, 'e'},
+    {6, 'f'}, {7, 'g'}, {8, 'h'}, {9, 'i'}, {10, 'j'}, {11, 'k'},
+    {12, 'l'}, {13, 'm'}, {14, 'n'}, {15, 'o'}, {16, 'p'}, {17, 'q'},
+    {18, 'r'}, {19, 's'}, {20, 't'}, {21, 'u'}, {22, 'v'}, {23, 'w'},
+    {24, 'x'}, {25, 'y'}, {26, 'z'}
+};
 
+// using ModelType = RTNeural::ModelT<float, vocab_size, embed_size,
+//     RTNeural::DenseT<float, vocab_size, embed_size>>;
+
+using ModelType = RTNeural::ModelT<float, vocab_size, vocab_size,
+    RTNeural::DenseT<float, vocab_size, embed_size>,
+    RTNeural::GRULayerT<float, embed_size, hidden_size>,
+    RTNeural::GRULayerT<float, hidden_size, hidden_size>,
+    RTNeural::DenseT<float, hidden_size, vocab_size>>;
 
 void loadModel(std::ifstream& jsonStream, ModelType& model)
 {
     nlohmann::json modelJson;
     jsonStream >> modelJson;
 
-    auto& gru1 = model.get<0>();
+    auto& embedding_ff = model.get<0>();
+    RTNeural::torch_helpers::loadDense<float> (modelJson, "embedding_ff.", embedding_ff, false);
+
+    auto& gru1 = model.get<1>();
     RTNeural::torch_helpers::loadGRU<float> (modelJson, "gru1.", gru1);
 
-    auto& gru2 = model.get<1>();
+    auto& gru2 = model.get<2>();
     RTNeural::torch_helpers::loadGRU<float> (modelJson, "gru2.", gru2);
 
-    auto& dense = model.get<2>();
-    RTNeural::torch_helpers::loadDense<float> (modelJson, "dense.", dense);
+    auto& fc = model.get<3>();
+    RTNeural::torch_helpers::loadDense<float> (modelJson, "fc.", fc);
+}
+
+std::vector<float> oneHotEncode(int value, int numClasses) {
+    // Create a vector of zeros with a size equal to the number of classes
+    std::vector<float> oneHot(numClasses, 0.0f);
+    
+    // Set the position corresponding to the value to 1
+    if (value >= 0 && value < numClasses) {
+        oneHot[value] = 1.0f;
+    }
+    
+    return oneHot;
+}
+
+float sample(float *logits) {
+    // for (size_t i = 0; i < vocab_size; ++i) {
+    //     std::cout << logits[i] << " ";
+    // }
+    // std::cout << std::endl;
+
+    float max = -10000;
+    size_t argmax = 0;
+    for (size_t i = 0; i < vocab_size; ++i) {
+        if (logits[i] > logits[argmax]) {
+            argmax = i;
+            max = logits[i];
+        }
+    }
+
+    return (float)argmax;
+}
+
+void print_char(float token) {
+    int tokeni = (int)token;
+    if (tokeni == 0) {
+        std::cout << ' ';
+    } else {
+        std::cout << (char)(tokeni + 96);
+    }
+    // std::cout << token << std::endl;
+}
+
+void generate(ModelType model, std::vector<float> sentence) {
+    float *output;
+
+    // std::cout << sentence.size() << std::endl;
+
+    for(size_t i = 0; i < sentence.size(); ++i)
+    {
+        // std::cout << sentence[i] << std::endl;
+        std::vector<float> oneHot = oneHotEncode(sentence[i], vocab_size);
+        model.forward(oneHot.data());
+        output = (float *)model.getOutputs();
+        float next_token = sample(output);
+
+        print_char(sentence[i]);
+    }
+
+    for(int i = 0; i < 12; ++i) {
+        std::vector<float> oneHot = oneHotEncode(sentence[sentence.size() - 1], vocab_size);
+        model.forward(oneHot.data());
+        output = (float *)model.getOutputs();
+        float next_token = sample(output);
+        sentence.push_back(next_token);
+
+        print_char(next_token);
+    }
 }
 
 int main([[maybe_unused]] int argc, char* argv[])
 {
     std::cout << "Running \"torch gru\" example..." << std::endl;
+
+    // for (int i = 0; i < 27; ++i) {
+    //     print_char((float)i);
+    // }
 
     auto executablePath = fs::weakly_canonical(fs::path(argv[0]));
     auto modelFilePath = getModelFile(executablePath);
@@ -76,22 +162,28 @@ int main([[maybe_unused]] int argc, char* argv[])
     model.reset();
 
     std::ifstream modelInputsFile { getInputFile(executablePath) };
-    const std::vector<float> inputs = load_csv::loadFile<float>(modelInputsFile);
-    std::vector<float> outputs {};
-    outputs.resize(inputs.size(), {});
+    std::vector<float> sentence = load_csv::loadFile<float>(modelInputsFile);
 
-    for(size_t i = 0; i < inputs.size(); ++i)
-    {
-        outputs[i] = model.forward(&inputs[i]);
-    }
+    generate(model, sentence);
 
-    std::ifstream modelOutputsFile { getOutputFile(executablePath) };
-    const std::vector<float> expected_y = load_csv::loadFile<float>(modelOutputsFile);
-    for(size_t i = 100; i < 105; ++i)
-    {
-        std::cout << expected_y[i] << " ";
-        std::cout << outputs[i] << std::endl;
-    }
+    
+    // std::vector<float> outputs {};
+    // outputs.resize(sentence.size(), {});
+
+    // for(size_t i = 0; i < inputs.size(); ++i)
+    // {
+    //     std::cout << inputs[i] << std::endl;
+    //     std::vector<float> oneHot = oneHotEncode(inputs[i], vocab_size);
+    //     outputs[i] = model.forward(oneHot.data());
+    // }
+
+    // std::ifstream modelOutputsFile { getOutputFile(executablePath) };
+    // const std::vector<float> expected_y = load_csv::loadFile<float>(modelOutputsFile);
+    // for(size_t i = 100; i < 105; ++i)
+    // {
+    //     std::cout << expected_y[i] << " ";
+    //     std::cout << outputs[i] << std::endl;
+    // }
 
     return 0;
 }
